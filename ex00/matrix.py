4da7a7b861a880c5aaf643ae9bb607a8a3883d25
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-04 19:14:19 +0900

    asdf

---
+++ ex00/matrix.py
@@ -0,0 +1,12 @@
+class Matrix:
+    def __init__(self, b: list):
+        matrix = b
+
+    def add(self, v):
+        pass
+
+    def sub(self, v):
+        pass
+
+    def scl(self, v):
+        pass
---
+++ ex00/vector.py
@@ -0,0 +1,14 @@
+class Vector:
+    def __init__(self, v: list):
+        self.v = v
+        self.size = len(list)
+    def add(self, v: 'Vector'):
+        if self.v.size != v.size:
+            raise "two vector size not equal"
+        pass
+
+    def sub(self, v):
+        pass
+
+    def scl(self, a):
+        pass
