ae3ebf7a5daea972b05a5c14a85d72a9af6708b6
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-07 19:08:56 +0900

    vector.py

--- ex00/vector.py
+++ ex00/vector.py
@@ -1,12 +1,12 @@
 class Vector:
     def __init__(self, v: list[float]):
-        self.v = v
+        self.vector = v
         self.size = len(v)
 
     def __str__(self):
         result = ""
         for i in range(self.size):
-            result += "[" + str(self.v[i]) + "]\n"
+            result += "[" + str(self.vector[i]) + "]\n"
         return result
             
 
@@ -14,19 +14,19 @@ class Vector:
         if self.size != v.size:
             raise "two vector size not equal"
         for i in range(self.size):
-            self.v[i] += v.v[i]
+            self.vector[i] += v.vector[i]
 
     def sub(self, v: 'Vector') -> None:
         if self.size != v.size:
             raise "two vector size not equal"
         for i in range(self.size):
-            self.v[i] -= v.v[i]
+            self.vector[i] -= v.vector[i]
 
     def scl(self, a: 'int') -> None:
         if not isinstance(a, float):
             raise TypeError
         for i in range(self.size):
-            self.v[i] *= a
+            self.vector[i] *= a
     
     def dot(self, v : 'Vector'):
         if self.size != v.size:
@@ -34,26 +34,26 @@ class Vector:
         
         value = 0.0
         for i in range(v.size):
-            value += (self.v[i] * v.v[i])
+            value += (self.vector[i] * v.vector[i])
         return value
     
     def norm_1(self):
         norm = 0.0
         for i in range(self.size):
-            norm += abs(self.v[i])
+            norm += abs(self.vector[i])
         return norm
     
     def norm(self):
         norm = 0.0
         for i in range(self.size):
-            norm += self.v[i] ** 2
+            norm += self.vector[i] ** 2
         return norm ** (0.5)
     
     def norm_inf(self):
         norm = 0.0
         for i in range(self.size):
-            if abs(self.v[i]) > norm:
-                norm = abs(self.v[i])
+            if abs(self.vector[i]) > norm:
+                norm = abs(self.vector[i])
         return norm
 
         
