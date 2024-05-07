ea9dd1231c99fa77ddea4c809ed98221cd8b4834
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-07 17:35:19 +0900

    exception check

--- ex06/cross_product.py
+++ ex06/cross_product.py
@@ -4,3 +4,16 @@ sys.path.append('..')
 from ex00.vector import Vector
 from ex00.matrix import Matrix
 
+def cross_product(u: 'Vector', v: 'Vector') -> 'Vector':
+    try:
+        if not isinstance(u, Vector):
+            raise TypeError("u is not vector")
+        if not isinstance(v, Vector):
+            raise TypeError("v is not vector")
+        if u.size != 3:
+            raise ValueError("u size is not 3")
+        if v.size != 3:
+            raise ValueError("v size is not 3")
+    except Exception as e:
+        print(e)
+        exit(1)
