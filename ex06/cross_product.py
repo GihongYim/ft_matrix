cf11dd140b7c5b94756818f07cb8594e7118c301
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-07 17:43:16 +0900

    asdf

--- ex06/cross_product.py
+++ ex06/cross_product.py
@@ -29,3 +29,11 @@ if __name__ == "__main__":
     u = Vector([0.0, 0.0, 1.0])
     v = Vector([1.0, 0.0, 0.0])
     print(cross_product(u, v))
+
+    u = Vector([1.0, 2.0, 3.0])
+    v = Vector([4.0, 5.0, 6.0])
+    print(cross_product(u, v))
+
+    u = Vector([4.0, 2.0, -3.0])
+    v = Vector([-2.0, -5.0, 16.0])
+    print(cross_product(u, v))
