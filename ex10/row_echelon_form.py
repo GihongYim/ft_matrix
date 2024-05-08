ad67b2b4e7bc7047f839c41f527dfde488b6809e
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-08 20:25:35 +0900

    asdf

--- ex10/row_echelon_form.py
+++ ex10/row_echelon_form.py
@@ -5,4 +5,16 @@ from ex00.vector import Vector
 from ex00.matrix import Matrix
 
 if __name__ == "__main__":
-    
+    u = Matrix([
+        [1., 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.],
+    ])
+    print(u.row_echelon())
+
+
+    u = Matrix([
+        [1.0, 2.0],
+        [3.0, 4.0]
+    ])
+    print(u.row_echelon())
