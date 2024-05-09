a181b6a0aa6913353278cd2999e7d0322b53e337
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-09 17:47:29 +0900

    determinant

--- ex11/determinant.py
+++ ex11/determinant.py
@@ -11,3 +11,32 @@ u = Matrix([
 
 print(u.determinant())
 print()
+
+u = Matrix([
+    [2., 0., 0.],
+    [0., 2., 0.],
+    [0., 0., 2.],
+])
+
+print(u.determinant())
+print()
+
+
+u = Matrix([
+    [8., 5., -2.],
+    [4., 7., 20.],
+    [7., 6., 1.],
+])
+
+print(u.determinant())
+print()
+
+u = Matrix([
+    [ 8., 5., -2., 4.],
+    [ 4., 2.5, 20., 4.],
+    [ 8., 5., 1., 4.],
+    [28., -4., 17., 1.],
+])
+
+print(u.determinant())
+print()
